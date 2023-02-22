341e362536a2ea8e64661c1220c5ae9f72555fb0
manish <ms5978307@gmail.com>
2023-02-22 19:33:35 +0530

    reset

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import Welcome from "./components/Welcome";
-import "./styles/app.scss";
-
 const App = () => {
     return (
         <>
-            <Welcome />
             <h1>Hello world</h1>
         </>
     );
