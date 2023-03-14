b5e7dc006a7a56f0019650337e2aa0154c88dbff
manish <ms5978307@gmail.com>
2023-03-14 18:48:28 +0530

    added different pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Career from "./pages/Career";
+import Overview from "./pages/Overview";
+import PageNotFound from "./pages/PageNotFound";
+import Projects from "./pages/Projects";
+
 const App = () => {
     return (
-        <>
-            <h1>Hello world</h1>
-        </>
+        <Routes>
+            <Route path="/" element={<Overview />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/career" element={<Career />} />
+            <Route path="*" element={<PageNotFound />} />
+        </Routes>
     );
 };
 
