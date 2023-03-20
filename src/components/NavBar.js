c12b549ea7867fafadc935781b2436fbf1635e60
manish <ms5978307@gmail.com>
2023-03-20 14:38:41 +0530

    added locomotive scroll

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,6 @@
 import { Link } from "react-router-dom";
 import MenuIcon from "../assets/icons/MenuIcon";
 
-const menuLinkList = [
-    {
-        title: "Overview",
-        path: "/",
-        isDownloadable: false,
-    },
-    {
-        title: "Projects",
-        path: "/projects",
-        isDownloadable: false,
-    },
-    {
-        title: "Career",
-        path: "/career",
-        isDownloadable: false,
-    },
-    {
-        title: "Resume",
-        isDownloadable: true,
-    },
-];
-
 const NavBar = ({ isMenuOpen, setIsMenuOpen }) => {
     return (
         <header id="nav-bar">
@@ -30,17 +8,6 @@ const NavBar = ({ isMenuOpen, setIsMenuOpen }) => {
                 <h1>
                     <Link to="/">System.out.println("Hello")</Link>
                 </h1>
-                <ul>
-                    {menuLinkList.map((menuLinkItem, key) => {
-                        return menuLinkItem.isDownloadable ? (
-                            <li key={key}>{menuLinkItem.title}</li>
-                        ) : (
-                            <Link key={key} to={menuLinkItem.path}>
-                                <li>{menuLinkItem.title}</li>
-                            </Link>
-                        );
-                    })}
-                </ul>
                 <div>
                     <MenuIcon
                         isMenuOpen={isMenuOpen}
