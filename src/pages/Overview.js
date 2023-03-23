fc2424ef6ac62452d28b1e472850cdfe9ac8384c
manish <ms5978307@gmail.com>
2023-03-23 10:35:24 +0530

    added banner component

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,27 +1,10 @@
-import { Link } from "react-router-dom";
+import Banner from "../components/Banner";
 import HorizontalScrollSection from "../components/HorizontalScrollSection";
 
 const Overview = () => {
     return (
         <section id="overview">
-            <section className="banner">
-                <article className="glass">
-                    <h2>Hi, i'am Manish</h2>
-                    <p>
-                        A tech enthusiast, currently working as Quality
-                        Assurance Engineer. I also work on some full stack
-                        projects and frontend projects.
-                    </p>
-                    <div className="btn-group">
-                        <Link to="/projects">
-                            <button>My Projects</button>
-                        </Link>
-                        <Link>
-                            <button>My Resume</button>
-                        </Link>
-                    </div>
-                </article>
-            </section>
+            <Banner />
             <HorizontalScrollSection />
         </section>
     );
