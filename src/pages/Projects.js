4d631d3d40dde8a3ab58fdbe0567b6a04e32ef0b
manish <ms5978307@gmail.com>
2023-03-30 19:50:14 +0530

    added filter tabs in projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
 const Projects = () => {
-    return <div>Projects</div>;
+    const [activeTab, setActiveTab] = useState(0);
+
+    const tagList = [
+        { name: "All", tabId: 0 },
+        { name: "Frontend", tabId: 1 },
+        { name: "Backend", tabId: 2 },
+        { name: "Database", tabId: 3 },
+    ];
+
+    return (
+        <section className="projects">
+            <div className="button-filter-group">
+                {tagList.map((tag, key) => (
+                    <button
+                        key={key}
+                        className={
+                            activeTab === tag.tabId
+                                ? "filter-button active"
+                                : "filter-button"
+                        }
+                        onClick={() => setActiveTab(tag.tabId)}
+                    >
+                        {tag.name}
+                    </button>
+                ))}
+            </div>
+        </section>
+    );
 };
 
 export default Projects;
