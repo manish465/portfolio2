d3895f6aa1b55ff794626665beb089f1dbf3d300
manish <ms5978307@gmail.com>
2023-03-30 10:54:16 +0530

    added links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,13 @@
 const Contact = () => {
-    return <section className="contact-section">contact</section>;
+    return (
+        <section className="contact-section">
+            <div className="links">
+                <div className="link">GitHub</div>
+                <div className="link">Gmail</div>
+                <div className="link">Linkedin</div>
+            </div>
+        </section>
+    );
 };
 
 export default Contact;
