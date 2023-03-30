aeb2cdba9842c8b8fa20aa53c8fc690ef0f7b3a4
manish <ms5978307@gmail.com>
2023-03-30 14:56:56 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import { Route, Routes } from "react-router-dom";
 import AppStructure from "./AppStructure";
+import AppContext from "./context/AppContext";
 
 import { pageList } from "./pages";
 
 const App = () => {
     return (
-        <AppStructure>
-            <main>
-                <Routes>
-                    {pageList.map((page, key) => (
-                        <Route
-                            key={key}
-                            path={page.path}
-                            element={page.element}
-                        />
-                    ))}
-                </Routes>
-            </main>
-        </AppStructure>
+        <AppContext>
+            <AppStructure>
+                <main>
+                    <Routes>
+                        {pageList.map((page, key) => (
+                            <Route
+                                key={key}
+                                path={page.path}
+                                element={page.element}
+                            />
+                        ))}
+                    </Routes>
+                </main>
+            </AppStructure>
+        </AppContext>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-import { StrictMode } from "react";
 import "./styles/main.scss";
 
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
-import AppContext from "./context/AppContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <StrictMode>
         <BrowserRouter>
-            <AppContext>
-                <App />
-            </AppContext>
+            <App />
         </BrowserRouter>
     </StrictMode>
 );
