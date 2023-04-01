8515601d820fb8e17e05581034b4161858038c8b
manish <ms5978307@gmail.com>
2023-04-01 13:31:45 +0530

    refactoring....

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ProjectCard from "../components/ProjectCard";
 import { projectDataList, tagDataList } from "../data";
 
 const Projects = () => {
@@ -23,34 +24,7 @@ const Projects = () => {
             </div>
             <div className="project-list">
                 {projectDataList.map((project, key) => (
-                    <div key={key} className="project">
-                        <img src={project.img} alt={project.name} />
-                        <h3 className="project-name">{project.name}</h3>
-                        <p className="project-description">
-                            {project.description}
-                        </p>
-                        <ul className="project-tags">
-                            {project.tags.map((tag, tagKey) => (
-                                <li key={tagKey} className="tag">
-                                    {"#" + tag}
-                                </li>
-                            ))}
-                        </ul>
-                        <a
-                            href={project.github}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <i class="fa-brands fa-github"></i>
-                        </a>
-                        <a
-                            href={project.liveSite}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <i class="fa-solid fa-up-right-from-square"></i>
-                        </a>
-                    </div>
+                    <ProjectCard key={key} project={project} />
                 ))}
             </div>
         </section>
