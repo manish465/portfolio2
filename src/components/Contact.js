8de621b3c6841a20d8251786cbd0878c6d7c0755
manish <ms5978307@gmail.com>
2023-04-03 11:11:02 +0530

    added links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,20 @@ const Contact = () => {
     return (
         <section className="contact-section">
             <div className="links">
-                <div className="link">GitHub</div>
-                <div className="link">Gmail</div>
-                <div className="link">Linkedin</div>
+                <a
+                    href="https://github.com/manish465"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <div className="link">GitHub</div>
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/manish-sahu-a956a3209/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <div className="link">Linkedin</div>
+                </a>
             </div>
         </section>
     );
