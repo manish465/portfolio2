1a3c735cce93a2d47c8d271f4700e5376d61a6ba
manish <ms5978307@gmail.com>
2024-07-21 21:01:48 +0530

    update data.js

--- src/data.js
+++ src/data.js
@@ -1,179 +1,207 @@
 export const skillData = {
-    heading: "Skills",
-    techList: [
-        {
-            name: "Java",
-            image: "https://img.icons8.com/fluency/256/java-coffee-cup-logo.png",
-        },
-        {
-            name: "Spring Boot",
-            image: "https://img.icons8.com/officel/256/spring-logo.png",
-        },
-        {
-            name: "My SQL",
-            image: "https://img.icons8.com/fluency/256/my-sql.png",
-        },
-        {
-            name: "React JS",
-            image: "https://img.icons8.com/clouds/256/react.png",
-        },
-        {
-            name: "Next JS",
-            image: "https://vercel.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F2Z9UUDbUy5A0dMhrhS2umH%2F4c3239c3bb12aaefa5008306566806dd%2FLogotype-Dark__1_.png&w=1920&q=75",
-        },
-        {
-            name: "Selenium",
-            image: "https://img.icons8.com/officel/256/selenium-test-automation.png",
-        },
-        {
-            name: "Maven",
-            image: "https://upload.wikimedia.org/wikipedia/commons/f/f5/Maven_logo.gif",
-        },
-        {
-            name: "TestNG",
-            image: "https://qatestlab.com/assets/Uploads/testng1.png",
-        },
-        {
-            name: "MongoDB",
-            image: "https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg",
-        },
-        {
-            name: "Testcafe",
-            image: "https://mherman.org/assets/img/blog/testcafe.png",
-        },
-        {
-            name: "Playwright",
-            image: "https://www.lambdatest.com/resources/images/learning-hub/Playwright_Testing_Tutorial.png",
-        },
-        {
-            name: "JavaScript",
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png?20120221235433",
-        },
-        {
-            name: "Node JS",
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/590px-Node.js_logo.svg.png?20170401104355",
-        },
-        {
-            name: "HTML",
-            image: "https://upload.wikimedia.org/wikipedia/commons/3/33/Hypertext_markup_language.png",
-        },
-        {
-            name: "CSS",
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/363px-CSS3_logo_and_wordmark.svg.png?20160530175649",
-        },
-    ],
+  heading: "Skills",
+  techList: [
+    {
+      name: "Java",
+      image: "https://img.icons8.com/fluency/256/java-coffee-cup-logo.png",
+    },
+    {
+      name: "Spring Boot",
+      image: "https://img.icons8.com/officel/256/spring-logo.png",
+    },
+    {
+      name: "My SQL",
+      image: "https://img.icons8.com/fluency/256/my-sql.png",
+    },
+    {
+      name: "React JS",
+      image: "https://img.icons8.com/clouds/256/react.png",
+    },
+    {
+      name: "Next JS",
+      image:
+        "https://vercel.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F2Z9UUDbUy5A0dMhrhS2umH%2F4c3239c3bb12aaefa5008306566806dd%2FLogotype-Dark__1_.png&w=1920&q=75",
+    },
+    {
+      name: "Selenium",
+      image: "https://img.icons8.com/officel/256/selenium-test-automation.png",
+    },
+    {
+      name: "Maven",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/f/f5/Maven_logo.gif",
+    },
+    {
+      name: "TestNG",
+      image: "https://qatestlab.com/assets/Uploads/testng1.png",
+    },
+    {
+      name: "MongoDB",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg",
+    },
+    {
+      name: "Testcafe",
+      image: "https://mherman.org/assets/img/blog/testcafe.png",
+    },
+    {
+      name: "Playwright",
+      image:
+        "https://www.lambdatest.com/resources/images/learning-hub/Playwright_Testing_Tutorial.png",
+    },
+    {
+      name: "JavaScript",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png?20120221235433",
+    },
+    {
+      name: "Node JS",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/590px-Node.js_logo.svg.png?20170401104355",
+    },
+    {
+      name: "HTML",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/3/33/Hypertext_markup_language.png",
+    },
+    {
+      name: "CSS",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/363px-CSS3_logo_and_wordmark.svg.png?20160530175649",
+    },
+  ],
 };
 
 export const toolsData = {
-    heading: "Tools",
-    techList: [
-        {
-            name: "VS Code",
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
-        },
-        {
-            name: "Intellij",
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/IntelliJ_IDEA_Icon.svg/512px-IntelliJ_IDEA_Icon.svg.png?20200803071016",
-        },
-        {
-            name: "Eclipse",
-            image: "https://logowik.com/content/uploads/images/eclipse5466.jpg",
-        },
-        {
-            name: "Figma",
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/640px-Figma-logo.svg.png",
-        },
-        {
-            name: "MySQL Workbench",
-            image: "https://i0.wp.com/deepinthecode.com/wp-content/uploads/2017/10/mysql_workbench.jpg?w=490&ssl=1",
-        },
-        {
-            name: "Mongodb Compass",
-            image: "https://i0.wp.com/www.kenwalger.com/blog/wp-content/uploads/2017/04/mongodb-compass-e1491607950242.png?fit=125%2C125&ssl=1",
-        },
-        {
-            name: "Adobe XD",
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Adobe_XD_CC_icon.svg/512px-Adobe_XD_CC_icon.svg.png?20210729021535",
-        },
-        {
-            name: "Chrome",
-            image: "https://logowik.com/content/uploads/images/google-chrome-2022-new5983.jpg",
-        },
-        {
-            name: "Git",
-            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/512px-Git-logo.svg.png?20160811101906",
-        },
-        {
-            name: "Postman",
-            image: "https://logowik.com/content/uploads/images/postman-api-platform6643.logowik.com.webp",
-        },
-    ],
+  heading: "Tools",
+  techList: [
+    {
+      name: "VS Code",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+    },
+    {
+      name: "Intellij",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/IntelliJ_IDEA_Icon.svg/512px-IntelliJ_IDEA_Icon.svg.png?20200803071016",
+    },
+    {
+      name: "Eclipse",
+      image: "https://logowik.com/content/uploads/images/eclipse5466.jpg",
+    },
+    {
+      name: "Figma",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Figma-logo.svg/640px-Figma-logo.svg.png",
+    },
+    {
+      name: "MySQL Workbench",
+      image:
+        "https://i0.wp.com/deepinthecode.com/wp-content/uploads/2017/10/mysql_workbench.jpg?w=490&ssl=1",
+    },
+    {
+      name: "Mongodb Compass",
+      image:
+        "https://i0.wp.com/www.kenwalger.com/blog/wp-content/uploads/2017/04/mongodb-compass-e1491607950242.png?fit=125%2C125&ssl=1",
+    },
+    {
+      name: "Adobe XD",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Adobe_XD_CC_icon.svg/512px-Adobe_XD_CC_icon.svg.png?20210729021535",
+    },
+    {
+      name: "Chrome",
+      image:
+        "https://logowik.com/content/uploads/images/google-chrome-2022-new5983.jpg",
+    },
+    {
+      name: "Git",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Git-logo.svg/512px-Git-logo.svg.png?20160811101906",
+    },
+    {
+      name: "Postman",
+      image:
+        "https://logowik.com/content/uploads/images/postman-api-platform6643.logowik.com.webp",
+    },
+  ],
 };
 
 export const tagDataList = [
-    { name: "All", tabId: 0 },
-    { name: "Frontend", tabId: 1 },
-    { name: "Backend", tabId: 2 },
-    { name: "Database", tabId: 3 },
-    { name: "Reactjs", tabId: 4 },
-    { name: "Nextjs", tabId: 5 },
+  { name: "All", tabId: 0 },
+  { name: "Frontend", tabId: 1 },
+  { name: "Backend", tabId: 2 },
+  { name: "Database", tabId: 3 },
+  { name: "Reactjs", tabId: 4 },
+  { name: "Nextjs", tabId: 5 },
 ];
 
 export const projectDataList = [
-    {
-        name: "To Do App",
-        github: "https://github.com/manish465/to-do-app-frontend",
-        liveSite: "https://to-do-app-fullstack-manish465.netlify.app/",
-        tags: [1, 2, 3, 4],
-        description: "A fullstack webapp that can track and manage your task.",
-    },
-    {
-        name: "To Do App Backend",
-        github: "https://github.com/manish465/to-do-app-server",
-        tags: [2, 3],
-        description: "A Nodejs server with mongodb database.",
-    },
-    {
-        name: "Contact Manager",
-        github: "https://github.com/manish465/react-contact-manager",
-        liveSite: "https://contact-manager-manish465.netlify.app/",
-        tags: [1, 2, 3, 4],
-        description:
-            "A fullstack webapp that can manage and store your contacts.",
-    },
-    {
-        name: "Contact Manager Backend",
-        github: "https://github.com/manish465/contact-manager",
-        tags: [2, 3],
-        description: "A spring-boot backend with MySql database",
-    },
-    {
-        name: "Portfolio Site",
-        github: "https://github.com/manish465/portfolio2",
-        liveSite: "https://my-portfolio-v2-manish465.netlify.app/",
-        tags: [1, 4],
-        description: "A fontend portfolio site",
-    },
-    {
-        name: "Next Fast Food App",
-        github: "https://github.com/manish465/next-fast-food-app",
-        liveSite: "https://next-fast-food-app.vercel.app/",
-        tags: [1, 2, 3, 5],
-        description:
-            "A fullstack webapp that allows users to browse menus and place orders",
-    },
-    {
-        name: "React Sorting Visualizer",
-        github: "https://github.com/manish465/sorting-visualizer",
-        liveSite: "https://sorting-visualizer-manish456.netlify.app/",
-        tags: [1, 4],
-        description: "A React app that allow visualize the sorting process",
-    },
-    {
-        name: "React Flood Fill Visualizer",
-        github: "https://github.com/manish465/sorting-visualizer",
-        liveSite: "https://flood-fill-visualization-manish465.netlify.app/",
-        tags: [1, 4],
-        description: "A React app that allow visualize the flood fill effect",
-    },
+  {
+    name: "Recipe Sharing Platform",
+    github: "https://github.com/manish465/recipe-sharing-platform",
+    tags: [2, 3],
+    description: "Spring boot micro service for recipe sharing",
+  },
+  {
+    name: "ECommerceMS",
+    github: "https://github.com/manish465/ECommerceMS",
+    tags: [2, 3],
+    description: "Spring boot micro service e-commerce",
+  },
+  {
+    name: "To Do App",
+    github: "https://github.com/manish465/to-do-app-frontend",
+    liveSite: "https://to-do-app-fullstack-manish465.netlify.app/",
+    tags: [1, 2, 3, 4],
+    description: "A fullstack webapp that can track and manage your task.",
+  },
+  {
+    name: "To Do App Backend",
+    github: "https://github.com/manish465/to-do-app-server",
+    tags: [2, 3],
+    description: "A Nodejs server with mongodb database.",
+  },
+  {
+    name: "Contact Manager",
+    github: "https://github.com/manish465/react-contact-manager",
+    liveSite: "https://contact-manager-manish465.netlify.app/",
+    tags: [1, 2, 3, 4],
+    description: "A fullstack webapp that can manage and store your contacts.",
+  },
+  {
+    name: "Contact Manager Backend",
+    github: "https://github.com/manish465/contact-manager",
+    tags: [2, 3],
+    description: "A spring-boot backend with MySql database",
+  },
+  {
+    name: "Portfolio Site",
+    github: "https://github.com/manish465/portfolio2",
+    liveSite: "https://my-portfolio-v2-manish465.netlify.app/",
+    tags: [1, 4],
+    description: "A frontend portfolio site",
+  },
+  {
+    name: "Next Fast Food App",
+    github: "https://github.com/manish465/next-fast-food-app",
+    liveSite: "https://next-fast-food-app.vercel.app/",
+    tags: [1, 2, 3, 5],
+    description:
+      "A fullstack webapp that allows users to browse menus and place orders",
+  },
+  {
+    name: "React Sorting Visualizer",
+    github: "https://github.com/manish465/sorting-visualizer",
+    liveSite: "https://sorting-visualizer-manish456.netlify.app/",
+    tags: [1, 4],
+    description: "A React app that allow visualize the sorting process",
+  },
+  {
+    name: "React Flood Fill Visualizer",
+    github: "https://github.com/manish465/sorting-visualizer",
+    liveSite: "https://flood-fill-visualization-manish465.netlify.app/",
+    tags: [1, 4],
+    description: "A React app that allow visualize the flood fill effect",
+  },
 ];
