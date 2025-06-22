c16dc536dba8575db8c6e239bc9333cf81fe2198
manish <ms5978307@gmail.com>
2025-06-22 21:26:28 +0530

    added new resume

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,26 +1,25 @@
 import { Link } from "react-router-dom";
 
 const Banner = () => {
-    return (
-        <section className="banner">
-            <article className="glass">
-                <h2>Hi, i'am Manish</h2>
-                <p>
-                    A tech enthusiast, currently working as Quality Assurance
-                    Engineer. I also work on some full stack projects and
-                    frontend projects.
-                </p>
-                <div className="btn-group">
-                    <Link to="/projects">
-                        <button>My Projects</button>
-                    </Link>
-                    <a href={require("../assets/Manish_Resume.pdf")}>
-                        <button>My Resume</button>
-                    </a>
-                </div>
-            </article>
-        </section>
-    );
+  return (
+    <section className='banner'>
+      <article className='glass'>
+        <h2>Hi, i'am Manish</h2>
+        <p>
+          A tech enthusiast, currently working as Quality Assurance Engineer. I
+          also work on some full stack projects and frontend projects.
+        </p>
+        <div className='btn-group'>
+          <Link to='/projects'>
+            <button>My Projects</button>
+          </Link>
+          <a href={require("../assets/Manish_Sahu_Software_Engineer.pdf")}>
+            <button>My Resume</button>
+          </a>
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default Banner;
--- src/components/MenuContent.js
+++ src/components/MenuContent.js
@@ -2,41 +2,41 @@ import { Link } from "react-router-dom";
 import MenuContentCard from "./MenuContentCard";
 
 const menuItemList = [
-    {
-        title: "Overview",
-        backgroundClassList: "background-pic pic-1",
-        isDownloadable: false,
-        path: "/",
-    },
-    {
-        title: "Projects",
-        backgroundClassList: "background-pic pic-2",
-        isDownloadable: false,
-        path: "/projects",
-    },
-    {
-        title: "Resume",
-        backgroundClassList: "background-pic pic-3",
-        isDownloadable: true,
-    },
+  {
+    title: "Overview",
+    backgroundClassList: "background-pic pic-1",
+    isDownloadable: false,
+    path: "/",
+  },
+  {
+    title: "Projects",
+    backgroundClassList: "background-pic pic-2",
+    isDownloadable: false,
+    path: "/projects",
+  },
+  {
+    title: "Resume",
+    backgroundClassList: "background-pic pic-3",
+    isDownloadable: true,
+  },
 ];
 
 const MenuContent = () => {
-    return (
-        <section id="menu-content">
-            {menuItemList.map((menuItem, key) => {
-                return menuItem.isDownloadable ? (
-                    <a href={require("../assets/Manish_Resume.pdf")}>
-                        <MenuContentCard key={key} menuItem={menuItem} />
-                    </a>
-                ) : (
-                    <Link key={key} to={menuItem.path}>
-                        <MenuContentCard key={key} menuItem={menuItem} />
-                    </Link>
-                );
-            })}
-        </section>
-    );
+  return (
+    <section id='menu-content'>
+      {menuItemList.map((menuItem, key) => {
+        return menuItem.isDownloadable ? (
+          <a href={require("../assets/Manish_Sahu_Software_Engineer.pdf")}>
+            <MenuContentCard key={key} menuItem={menuItem} />
+          </a>
+        ) : (
+          <Link key={key} to={menuItem.path}>
+            <MenuContentCard key={key} menuItem={menuItem} />
+          </Link>
+        );
+      })}
+    </section>
+  );
 };
 
 export default MenuContent;
