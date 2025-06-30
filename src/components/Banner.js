b7e3661f402ee953cbb9f06670dcbca594465a35
manish <ms5978307@gmail.com>
2025-06-30 12:21:21 +0530

    Update banner text and expand skills/tools data
    Revised the Banner component text to better reflect current interests and expertise. Expanded skillData and toolsData in data.js with additional technologies and tools, and updated projectDataList with new projects and improved descriptions.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,25 +1,28 @@
 import { Link } from "react-router-dom";
 
 const Banner = () => {
-  return (
-    <section className='banner'>
-      <article className='glass'>
-        <h2>Hi, i'am Manish</h2>
-        <p>
-          A tech enthusiast, currently working as Quality Assurance Engineer. I
-          also work on some full stack projects and frontend projects.
-        </p>
-        <div className='btn-group'>
-          <Link to='/projects'>
-            <button>My Projects</button>
-          </Link>
-          <a href={require("../assets/Manish_Sahu_Software_Engineer.pdf")}>
-            <button>My Resume</button>
-          </a>
-        </div>
-      </article>
-    </section>
-  );
+    return (
+        <section className='banner'>
+            <article className='glass'>
+                <h2>Hi, i'am Manish</h2>
+                <p>
+                    Tech enthusiast and Power Programmer, passionate about
+                    building AL/ML, full-stack and frontend projects in my spare
+                    time.
+                </p>
+                <div className='btn-group'>
+                    <Link to='/projects'>
+                        <button>My Projects</button>
+                    </Link>
+                    <a
+                        href={require("../assets/Manish_Sahu_Software_Engineer.pdf")}
+                    >
+                        <button>My Resume</button>
+                    </a>
+                </div>
+            </article>
+        </section>
+    );
 };
 
 export default Banner;
